8ac4d4d002156e80b2bb6eded8273f70fc002e28
Sergei Afanasenko <afanasenko.s@gmail.com>
2020-05-20 21:11:12 +0300

    added new script

---
+++ update-groups-descr-and-notes.ps1
@@ -0,0 +1,14 @@
+﻿# Сергей Афанасенко 20/05/2020
+# Скрипт импортирует данные для заполнения из CSV-файла.
+# Файл CSV должен быть формата DOS (с ";" в качестве разделителя)
+# Столбцы, которые забираются из файла:
+# GroupName	 ResourceName	Owner	Order
+# 
+
+cls
+$groups = import-csv -Path $PSScriptRoot\change.csv -Encoding OEM -Delimiter ";"
+
+ForEach ($group in $groups){
+                $NewDescription = $group.Owner + "; " + $group.Order; Get-AdGroup $group.GroupName | `
+                Set-ADGroup -Description $NewDescription -Replace @{info=$group.ResourceName}
+                }
